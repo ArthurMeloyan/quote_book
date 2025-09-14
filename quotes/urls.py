c74de4fce3d00a112704a4ce1a2e8d44acf4bbb4
ArthurMeloyan <meloyan.12@mail.ru>
2025-09-15 00:50:43 +0400

    style: some style changes

--- quotes/urls.py
+++ quotes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.random_quote_view, name='random_quote'),
     path('quote/<int:quote_id>/like/', views.like, name='like_quote'),
     path('quote/<int:quote_id>/dislike/', views.dislike, name='dislike_quote'),
+    path('quote/create', views.add_quote_view, name='create_quote')
 ]
