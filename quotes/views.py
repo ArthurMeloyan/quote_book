f2264879228f78a0879867c38b27e6151ff26829
ArthurMeloyan <meloyan.12@mail.ru>
2025-09-14 21:08:11 +0400

    feat: Add function for random quote view

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
-
+from .models import Quote
+import random
 # Create your views here.
+
+page_views = 0  # views counter
+
+def random_quote_view(request):
+    global page_views
+    page_views += 1
+
+    # Get all quotes
+    quotes = Quote.objects.all()
+
+    # Check if there are no quotes at all
+    if not quotes:
+        context = {'page_views': page_views}
+        return render(request, 'quotes/index.html', context)
+    
+    # using weight for random quote view
+    total_weight = sum(q.weight for q in quotes)
+    random_num = random.uniform(0, total_weight)
+
+    cumulative_weight = 0
+    selected_quote = None
+
+    for quote in quotes:
+        cumulative_weight += quote.weight
+        if random_num <= cumulative_weight:
+            selected_quote = quote
+            break  # Found a "winner" so can breake 
+    
+    # Update views counter for quote
+    if selected_quote:
+        selected_quote.views += 1
+        selected_quote.save(update_fields=['views']) 
+
+    context = {
+        'quote': selected_quote,
+        'page_views': page_views,
+    }
+
+    return render(request, 'quotes/index.html', context)
