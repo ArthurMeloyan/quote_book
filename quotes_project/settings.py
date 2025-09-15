48930a7ea851ef2c9c36bd3a4e5ac37a37ce5666
ArthurMeloyan <meloyan.12@mail.ru>
2025-09-15 12:23:34 +0400

    chore: Add pythonanywhere host

--- quotes_project/settings.py
+++ quotes_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'artmel08.pythonanywhere.com']
 
 
 # Application definition
